d4edba0fdb8cbd34f9610fb2f728cd3f186b10ca
Pavel <>
2018-05-15 00:10:01 +0300

    добавил стандартные файлы

---
+++ english-test/english-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  english-test
+//
+//  Created by Pavel on 15/05/2018.
+//  Copyright © 2018 Pavel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
