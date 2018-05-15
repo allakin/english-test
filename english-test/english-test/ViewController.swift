621887b8f939bf5fd44f7734fac9f3727caee308
Pavel <>
2018-05-16 00:58:16 +0300

    добавил логику определения верного ответа

--- english-test/english-test/ViewController.swift
+++ english-test/english-test/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
 	var question = QuestionBank()
 	var skill = EnglishSkill.init()
 	var numberArray = 0
+	var correcrAnswerNumber = 1
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -32,9 +33,27 @@ class ViewController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
+	
 	@IBAction func nextQuestion(_ sender: AnyObject) {
-		
+		if numberArray < question.questionsList.count - 1 {
+			guard let buttonTitle = sender.title(for: .normal) else {return}
+			if buttonTitle == question.questionsList[numberArray].answer {
+				numberArray += 1
+				correcrAnswerNumber += 1
+				firstQuestion()
+				print("correct")
+			} else {
+				numberArray += 1
+				firstQuestion()
+				print("wrong")
+			}
+		} else {
+			let alert = UIAlertController(title: "Ура, Вы справились!", message: "\n Количество верных ответов: \(correcrAnswerNumber). \n Ваш уровень: \(skillSetting()).", preferredStyle: .alert)
+			alert.addAction(UIAlertAction(title: "Начать сначала", style: .default, handler: { action in
+				self.resetValue()
+			}))
+			self.present(alert, animated: true, completion: nil)
+		}
 	}
 	
 	private func firstQuestion() {
@@ -43,6 +62,28 @@ class ViewController: UIViewController {
 		buttonB.setTitle(question.questionsList[numberArray].buttonB, for: .normal)
 		buttonC.setTitle(question.questionsList[numberArray].buttonC, for: .normal)
 		numberQuestion.text = "Количество вопросов: \(numberArray + 1) / \(question.questionsList.count)"
+	}
+	
+	private func skillSetting () -> String {
+		var skills = ""
+		if correcrAnswerNumber <= 10 {
+			skills = skill.elementary
+		} else if correcrAnswerNumber >= 11 && correcrAnswerNumber <= 20 {
+			skills = skill.preIntermediate
+		} else if correcrAnswerNumber >= 21 && correcrAnswerNumber <= 30 {
+			skills = skill.intermediate
+		} else if correcrAnswerNumber >= 31 && correcrAnswerNumber <= 40 {
+			skills = skill.upperIntermediate
+		} else if correcrAnswerNumber >= 41 && correcrAnswerNumber <= 50 {
+			skills = skill.advanced
+		}
+		return skills
+	}
+	
+	private func resetValue() {
+		numberArray = 0
+		correcrAnswerNumber = 1
+		firstQuestion()
 	}
 	
 }
