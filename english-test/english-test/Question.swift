1181316ce1fac38f10f7ff2de5d73b54134d15b9
Pavel <>
2018-05-16 00:01:15 +0300

    создал класс вопросов

---
+++ english-test/english-test/Question.swift
@@ -0,0 +1,35 @@
+//
+//  Question.swift
+//  english-test
+//
+//  Created by Pavel on 15/05/2018.
+//  Copyright © 2018 Pavel. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+	let question: String
+	let	buttonA: String
+	let	buttonB: String
+	let	buttonC: String
+	let	answer: String
+	init(questionLabel: String, answerA: String, answerB: String, answerC: String, correctAnswer: String) {
+		self.question = questionLabel
+		self.buttonA = answerA
+		self.buttonB = answerB
+		self.buttonC = answerC
+		self.answer = correctAnswer
+	}
+}
+
+class QuestionBank {
+	var questionsList = [Question]()
+	init() {
+		questionsList.append(Question(questionLabel: "They _____ from Spain.", answerA: "is", answerB: "are", answerC: "do", correctAnswer: "are"))
+		questionsList.append(Question(questionLabel: "Peter _____ at seven o’clock.", answerA: "goes up", answerB: "gets", answerC: "gets up", correctAnswer: "gets up"))
+		questionsList.append(Question(questionLabel: "_____ you like this DVD?", answerA: "Are", answerB: "Have", answerC: "Do", correctAnswer: "Do"))
+		questionsList.append(Question(questionLabel: "What do you do? I’m _____ student.", answerA: "the", answerB: "a", answerC: "is", correctAnswer: "the"))
+	}
+}
+
