a724e1f842deef6c38b3862d5033a4b72d0224d0
Pavel <>
2018-05-15 23:51:58 +0300

    создал ui и подключил элементы

--- english-test/english-test/ViewController.swift
+++ english-test/english-test/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet weak var questionLabel: UILabel!
+	@IBOutlet weak var numberQuestion: UILabel!
+	@IBOutlet weak var buttonA: UIButton!
+	@IBOutlet weak var buttonB: UIButton!
+	@IBOutlet weak var buttonC: UIButton!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,9 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func nextQuestion(_ sender: AnyObject) {
+		
+	}
 
 }
 
