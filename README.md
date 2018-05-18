ebcfb6d0ee50ca080cc61268fbff2ae78cbce7d5
Pavel <>
2018-05-18 21:31:51 +0300

    редактирование .md файла

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # english-test
+
+### Об приложении
+фывфывфывфыв
+
+### Скриншоты экрана 
+<img src="https://raw.githubusercontent.com/allakin/english-test/master/Media/0.png" width="804"> <img src="https://raw.githubusercontent.com/allakin/english-test/master/Media/1.png" width="804"> <img src="https://raw.githubusercontent.com/allakin/english-test/master/Media/2.png" width="804"> <img src="https://raw.githubusercontent.com/allakin/english-test/master/Media/3.png" width="804">
+
+### Видео
+
+[![Vimeo](https://raw.githubusercontent.com/bystritskiy/HardcoreTap/master/Media/youtube.png)](https://www.youtube.com/watch?v=6XC2gn-wFDI "Demo")
--- english-test/english-test/Question.swift
+++ english-test/english-test/Question.swift
@@ -26,10 +26,26 @@ class Question {
 class QuestionBank {
 	var questionsList = [Question]()
 	init() {
+		// 1
 		questionsList.append(Question(questionLabel: "They _____ from Spain.", answerA: "is", answerB: "are", answerC: "do", correctAnswer: "are"))
+		// 2
+		questionsList.append(Question(questionLabel: "What do you do? I’m _____ student.", answerA: "the", answerB: "a", answerC: "is", correctAnswer: "a"))
+		// 3
 		questionsList.append(Question(questionLabel: "Peter _____ at seven o’clock.", answerA: "goes up", answerB: "gets", answerC: "gets up", correctAnswer: "gets up"))
-		questionsList.append(Question(questionLabel: "_____ you like this DVD?", answerA: "Are", answerB: "Have", answerC: "Do", correctAnswer: "Do"))
-		questionsList.append(Question(questionLabel: "What do you do? I’m _____ student.", answerA: "the", answerB: "a", answerC: "is", correctAnswer: "the"))
+		// 4
+		questionsList.append(Question(questionLabel: "_____ you like this DVD?", answerA: "Are", answerB: "Have", answerC: "Do", correctAnswer: "gets up"))
+		// 5
+		questionsList.append(Question(questionLabel: "Have you _____ a car?", answerA: "any", answerB: "have", answerC: "got", correctAnswer: "got"))
+		// 6
+		questionsList.append(Question(questionLabel: "We don’t have _____ butter.", answerA: "a", answerB: "any", answerC: "got", correctAnswer: "any"))
+		// 7
+		questionsList.append(Question(questionLabel: "_____ some money here.", answerA: "There’re", answerB: "There", answerC: "There’s", correctAnswer: "There’s"))
+		// 8
+		questionsList.append(Question(questionLabel: "We _____ got a garage.", answerA: "haven’t", answerB: "hasn’t", answerC: "don’t", correctAnswer: "haven’t"))
+		// 9
+		questionsList.append(Question(questionLabel: "They _____ at home yesterday.", answerA: "was", answerB: "are", answerC: "were", correctAnswer: "were"))
+		// 10
+		questionsList.append(Question(questionLabel: "I _____ there for a long time.", answerA: "lived", answerB: "living", answerC: "live", correctAnswer: "lived"))
 	}
 }
 
